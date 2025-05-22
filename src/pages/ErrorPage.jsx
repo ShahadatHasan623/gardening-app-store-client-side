7a5aa1a7853b851d69d392765195ab9b0590e2ae
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-23 01:11:19 +0600

    updated header

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,7 +7,6 @@ const ErrorPage = () => {
       <h1 className="text-[8rem] font-bold animate-bounce">404</h1>
       <p className="text-xl mt-4 animate-fade-in">Oops! Page not found</p>
       <NavLink to='/'
-         
         className="mt-6 px-6 py-2 bg-white text-gray-900 rounded-md shadow hover:bg-red-500 hover:text-white transition-all duration-300"
       >
         Go to Home
